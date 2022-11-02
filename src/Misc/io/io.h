f974b3a6f0522704b65e22d58a79ec5f23267554
pkmntrainerfrost <smaarif.achsien@gmail.com>
2022-11-02 13:14:03 +0700

    Update io.h

--- src/Misc/io/io.h
+++ src/Misc/io/io.h
@@ -11,27 +11,38 @@
 #define VALID_INPUT 0;
 #define INVALID_INPUT 1;
 
+char ord(char c);
+
+char alphabeticalOrd(char c);
+
+char lower(char c);
+
+char upper(char c);
+
 boolean isUppercase(char c);
 
 boolean isLowercase(char c);
 
-boolean isCharAlpha(char c);
+boolean isAlpha(char c);
 
-boolean isCharNumeric(char c);
+boolean isNumeric(char c);
 
-boolean isAlpha(char *s);
+boolean isStringAlpha(char *s);
 
-boolean isNumeric(char *s);
+boolean isStringNumeric(char *s);
 
-boolean isAlphaNumeric(char *s);
+boolean isStringAlphaNumeric(char *s);
 
-int stringLength(char *s);
+int charToInt(char c);
 
+/* Meminta user untuk memasukkan sebuah integer. Akan mengembalikan INVALID_INPUT bila masukan tidak valid. */
 int intInput(int *i);
 
-int charInput(char *c);
-
 /* Meminta user untuk memasukkan sebuah string dengan panjang (length) tertentu. Apabila jumlah karakter melebihi panjang, input akan dipotong. Hasil input disimpan di s. */
+/* DEKLARASIKAN STRING TERLEBIH DAHULU DENGAN KAPASITAS LENGTH + 1!!!! */
 int stringInput(char *s, int length);
+
+/* Mengembalikan panjang string. */
+int stringLength(char *s);
 
 #endif
