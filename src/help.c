67c3f7cf53d840e83ded5008d5619ac7c3ad8323
Erensiratu <91366136+Erensiratu@users.noreply.github.com>
2022-11-02 22:34:22 +0700

    Add files via upload

---
+++ src/help.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void help(){
+    printf("berikut command-command yang tersedia:\n");
+    printf("1. START (membaca file konfigurasi default)\n");
+    printf("2. LOAD (diinputkan file yang akan dibaca)\n");
+    printf("3. SAVE (menyimpan state game pemain saat ini)\n");
+    printf("4. CREATEGAME (menambahkan game baru pada daftar game)\n");
+    printf("5. LISTGAME (menampilkan daftar game yang disediakan oleh sistem)\n");
+    printf("6. DELETEGAME (menghapus sebuah game dari daftar game)\n");
+    printf("7. QUEUEGAME (mendaftarkan permainan kedalam list)\n");
+    printf("8. PLAYGAME (memainkan sebuah permainan)\n");
+    printf("9. SKIPGAME (melewatkan permainan sebanyak n)\n");
+    printf("10. QUIT (keluar dari program)\n");
+}
