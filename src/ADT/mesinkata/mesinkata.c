47fa4b7e29788a8df14add4a08940e4f3964639e
pkmntrainerfrost <smaarif.achsien@gmail.com>
2022-11-02 13:02:52 +0700

    mesinkata
    kemungkinan besar bakal direvisi tergantung jawaban qna

--- src/ADT/mesinkata/mesinkata.c
+++ src/ADT/mesinkata/mesinkata.c
@@ -14,7 +14,39 @@ void ignoreBlank() {
 void startKata() {
 
     start();
-    ignoreBlank
+    ignoreBlank();
 
+    if (cc == MARK) {
+        endKata = true;
+    } else {
+        endKata = false;
+        salinKata();
+    }
+
+}
+
+void advKata() {
+
+    ignoreBlank();
+
+    if (cc == MARK) {
+        endKata = true;
+    } else {
+        salinKata();
+    }
+
+}
+
+void salinKata() {
+
+    int i = 0;
+
+    while ((cc != MARK) && (cc != BLANK)) {
+        currentKata.buffer[i] = cc;
+        adv();
+        i = i + 1;
+    }
+
+    currentKata.length = i;
 
 }
