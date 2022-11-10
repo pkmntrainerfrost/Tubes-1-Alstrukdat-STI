6320dea75b2fe0eb49a6e8aec54b0004031b0727
raniakiranaa <16521042@std.stei.itb.ac.id>
2022-11-11 03:45:48 +0700

    fix save

--- src/Utility/save/save.c
+++ src/Utility/save/save.c
@@ -9,7 +9,6 @@ void savetoFile(char saveFilename[], List L)
     fprintf(saveFile, "%d\n", L.nEff);
     int i;
     for(i=0; i<length(L); i++){
-        fprintf(saveFile, "%d. ", i+1);
         int j;
         for(j=0; j<L.A[i].length; j++){
             fprintf(saveFile, "%c", L.A[i].buffer[j]);
@@ -21,21 +20,21 @@ void savetoFile(char saveFilename[], List L)
     printf("Save file berhasil disimpan\n");
 }
 
-// int main()
-// {
-//     List L;
-//     createList(&L);
-//     insertLast(&L, stringToWord("RNG"));
-//     insertLast(&L, stringToWord("Diner DASH"));
-//     insertLast(&L, stringToWord("DINOSAUR IN EARTH"));
-//     insertLast(&L, stringToWord("RISEWOMAN"));
-//     insertLast(&L, stringToWord("EIFFEL TOWER"));
-//     insertLast(&L, stringToWord("Battleship")); 
-//     insertLast(&L, stringToWord("Tetris"));
-//     displayList(L);
-//     // blm bikin scanf buat saveFilename
-//     savetoFile("test.txt", L);
-//     return 0;
-// }
+int main()
+{
+    List L;
+    createList(&L);
+    insertLast(&L, stringToWord("RNG"));
+    insertLast(&L, stringToWord("Diner DASH"));
+    insertLast(&L, stringToWord("DINOSAUR IN EARTH"));
+    insertLast(&L, stringToWord("RISEWOMAN"));
+    insertLast(&L, stringToWord("EIFFEL TOWER"));
+    insertLast(&L, stringToWord("Battleship")); 
+    insertLast(&L, stringToWord("Tetris"));
+    displayList(L);
+    // blm bikin scanf buat saveFilename
+    savetoFile("test.txt", L);
+    return 0;
+}
 
-//compile: gcc utility/save.c ADT/list/array.c ADT/word/word.c ADT/word/mesinkata/mesinkata.c ADT/word/mesinkarakter/mesinkarakter.c Misc/io/io.c Misc/ascii/ascii.c -o driver
+//compile: gcc utility/save/save.c ADT/list/array.c ADT/word/word.c ADT/word/mesinkata/mesinkata.c ADT/word/mesinkarakter/mesinkarakter.c Misc/io/io.c Misc/ascii/ascii.c -o driver
