752d663062fa3e5e413f7ea3ddf94c349f4a74c5
pkmntrainerfrost <smaarif.achsien@gmail.com>
2022-11-10 14:36:19 +0700

    io

--- src/Misc/io/io.c
+++ src/Misc/io/io.c
@@ -3,9 +3,61 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "io.h"
 #include <math.h>
 
+#include "io.h"
+#include "../../ADT/list/array.h"
+#include "../../ADT/word/word.h"
+#include "../../ADT/word/mesinkata/mesinkata.h"
+
+#define INVALID_INPUT 1
+#define VALID_INPUT 0
+
+int wordInput(Word *w, int min, int max) {
+
+    createWord(w);
+
+    start(false,"");
+
+    int i = 0;
+    while (!eoi) {
+        if (i < N_MAX && i < max) {
+            w->buffer[i] = cc;
+            i = i + 1;
+        }
+        adv();
+    }
+
+    wordLength(*w) = i; 
+
+    if (wordLength(*w) < min) {
+        return INVALID_INPUT;
+    } else {
+        return VALID_INPUT;
+    }
+
+}
+
+int multiWordInput(List *input, int min, int count) {
+
+    deallocateList(input);
+    createList(input);
+
+    startKata(false,"");
+
+    int i = 0;
+    while (!endKata && i < count) {
+        insertAt(input,currentKata,0);
+        advKata;
+    }
+
+    if (length(*input) < min) {
+        return INVALID_INPUT;
+    } else {
+        return VALID_INPUT;
+    }
+
+}
 
 
 // /* TYPE CONVERSION */
--- src/Misc/io/io.h
+++ src/Misc/io/io.h
@@ -8,8 +8,14 @@
 
 #include "../../ADT/boolean.h"
 
-#define VALID_INPUT 0;
-#define INVALID_INPUT 1;
+#define INVALID_INPUT 0
+#define VALID_INPUT 1
+
+/* Meminta user untuk menginput satu buah kata dengan panjang minimal min dan panjang maksimal max, lalu menyimpannya di w. Kata dianggap berakhir ketika bertemu dengan karakter newline. Apabila input melebihi max atau N_MAX, karakter yang lebih akan diabaikan. Apabila input kurang dari min, akan mengembalikan INVALID_INPUT. */
+int wordInput(Word *w, int min, int max);
+
+/* Meminta user untuk menginput beberapa kata dengan banyak minimal min dan banyak maksimal max, lalu menyimpannya di array of kata input. Input dianggap berakhir ketika bertemu dengan karakter newline, dan kata dipisahkan berdasarkan spasi. Apabila input melebihi max, kata yang lebih akan diabaikan. Apabila input kurang dari min, akan mengembalikan INVALID_INPUT. */
+int multiWordInput(List *input, int min, int count);
 
 // int charToInt(char c);
 
