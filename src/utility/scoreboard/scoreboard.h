0c383f167d2ec7f24bd4a8d972ac80bd2d956df3
Erensiratu <16521236@std.stei.itb.ac.id>
2022-11-23 11:40:31 +0700

    ok

---
+++ src/utility/scoreboard/scoreboard.h
@@ -0,0 +1,16 @@
+#ifndef SCOREBOARD_H
+#define SCOREBOARD_H
+
+#include <stdio.h>
+#include "../../ADT/map/map.h"
+#include "../../ADT/set/set.h"
+#include "../../ADT/word/mesinkata/mesinkata.h"
+#include "../../ADT/word/word.h"
+#include "../listGame/listGame.h"
+#include "../../Misc/io/io.h"
+#include "../../Misc/ascii/ascii.h"
+
+void scoreboard(Map daftarsb, int num);
+void resetscoreboard(Map *daftarsb);
+
+#endif
