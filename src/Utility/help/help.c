de2d4837cfd2c13578c3f1c7cc246b95d7c7e5ad
raniakiranaa <16521042@std.stei.itb.ac.id>
2022-11-28 15:52:00 +0700

    update help

--- src/Utility/help/help.c
+++ src/Utility/help/help.c
@@ -23,14 +23,18 @@ void help(){
     header();
 
     printf("Berikut command-command yang tersedia:\n");
-    printf("1. SAVE - Menyimpan state game pemain saat ini ke dalam suatu file\n");
-    printf("2. CREATE GAME - Menambahkan game baru pada daftar game\n");
-    printf("3. LIST GAME - Menampilkan daftar game yang disediakan oleh sistem\n");
-    printf("4. DELETE GAME - Menghapus sebuah game dari daftar game\n");
-    printf("5. QUEUE GAME - Mendaftarkan permainan ke dalam list\n");
-    printf("6. PLAY GAME - Memainkan sebuah permainan di urutan pertama antrian game\n");
-    printf("7. SKIP GAME <N> - Melewatkan permainan dalam antrian game sebanyak N\n");
-    printf("8. QUIT - Keluar dari program\n");
+    printf("1.  CREATE GAME - Menambahkan game baru pada daftar game\n");
+    printf("2.  LIST GAME - Menampilkan daftar game yang disediakan oleh sistem\n");
+    printf("3.  DELETE GAME - Menghapus sebuah game dari daftar game\n");
+    printf("4.  QUEUE GAME - Mendaftarkan permainan ke dalam list\n");
+    printf("5.  PLAY GAME - Memainkan sebuah permainan di urutan pertama antrian game\n");
+    printf("6.  SKIP GAME <N> - Melewatkan permainan dalam antrian game sebanyak N\n");
+    printf("7.  SCOREBOARD - Menampilkan nama dan skor untuk semua game\n");
+    printf("8.  RESET SCOREBOARD - Melakukan reset terhadap scoreboard permainan\n");
+    printf("9.  HISTORY <N> - Melihat sebanyak N game yang baru dimainkan\n");
+    printf("10. RESET HISTORY - Menghapus semua history permainan yang telah dimainkan\n");
+    printf("11. SAVE <filename>.txt - Menyimpan state game pemain saat ini ke dalam file filename\n");
+    printf("12. QUIT - Keluar dari program\n");
 
     printf("\nTekan [ENTER] untuk kembali ke menu utama...");
 
