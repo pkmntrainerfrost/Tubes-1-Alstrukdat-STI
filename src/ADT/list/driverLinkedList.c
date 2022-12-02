8f52224f992e7be8a29360fd6696f566366d8dcb
raniakiranaa <16521042@std.stei.itb.ac.id>
2022-12-02 18:45:47 +0700

    driver linked list

--- src/ADT/list/driverLinkedList.c
+++ src/ADT/list/driverLinkedList.c
@@ -3,26 +3,48 @@
 
 int main()
 {
+    printf("hai");
     LinkedList LL;
     createLL(&LL);
 
+    printf("halo");
     Point P;
     createPoint(&P, 1, 2);
 
+    printf("wuh");
     Address x = allocateNode(P);
     insertFirstLL(&LL, x);
 
+    printf("yew");
     createPoint(&P, 3, 4);
+
+    printf("haiii");
     insVLastLL(&LL, P);
 
-    deleteLastLL(&LL, &x);
+    printf("hehe");
+    // deleteLastLL(&LL, &x);
+    deleteFirstLL(&LL, &x);
     P = getElmtLL(LL, 1);
 
+    printf("hashda");
     setElmtLL(&LL, 1, P);
 
+    printf("llele");
     Address y = searchLL(LL, P);
+    if (y != NIL) {
+        printf("P ada di LL\n");
+    } else {
+        printf("P tidak ada di LL\n");
+    }
+
+    printf("hai");
 
     printf("%d\n", lengthLL(LL));
 
+    // gajalan
+    printf("%d %d\n", ABSCISSA(getElmtLL(LL, lengthLL(LL))), ORDINATE(getElmtLL(LL, lengthLL(LL))));
+
     return 0;
 }
+
+// compile : gcc src/ADT/point/point.c src/ADT/list/linkedList.c src/ADT/list/driverLinkedList.c -o driver
